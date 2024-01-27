e0d285aa8c89ac6d6f2b3b7a0a17118459391509
UncensorX1 <155687381+UncensorX1@users.noreply.github.com>
2024-01-27 19:51:25 +0000

    Create hy.php

---
+++ hy.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Read Text File</title>
+    <style>
+        body {
+            font-family: "Courier New", monospace;
+        }
+    </style>
+</head>
+<body>
+    <h1>Read Text File</h1>
+    <pre id="content"></pre>
+
+    <script>
+        fetch('Hy2F.txt')
+            .then(response => response.text())
+            .then(data => {
+                var preElement = document.getElementById("content");
+                preElement.textContent = data;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    </script>
+</body>
+</html>
