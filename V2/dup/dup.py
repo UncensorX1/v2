175ce50a79f8c2d8166fcefe55c5795caf8751c9
UncensorX1 <155687381+UncensorX1@users.noreply.github.com>
2024-02-02 11:58:56 +0000

    1

--- V2/dup/dup.py
+++ V2/dup/dup.py
@@ -2,13 +2,13 @@ import hashlib
 
 input_file_path = "V2/Ss.txt"
 output_file_path_1 = "SsL.txt"
-output_file_path_2 = "SsF.txt"
+# output_file_path_2 = "SsF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
 output_file_1 = open(output_file_path_1, "w")
-output_file_2 = open(output_file_path_2, "w")
+# output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
     hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
@@ -18,7 +18,7 @@ for line in input_file:
         completed_lines_hash.add(hashValue)
 
 output_file_1.close()
-output_file_2.close()
+# output_file_2.close()
 input_file.close()
 
 ####
@@ -27,13 +27,13 @@ import hashlib
 
 input_file_path = "V2/Ssr.txt"
 output_file_path_1 = "SsrL.txt"
-output_file_path_2 = "SsrF.txt"
+# output_file_path_2 = "SsrF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
 output_file_1 = open(output_file_path_1, "w")
-output_file_2 = open(output_file_path_2, "w")
+# output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
     hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
@@ -43,7 +43,7 @@ for line in input_file:
         completed_lines_hash.add(hashValue)
 
 output_file_1.close()
-output_file_2.close()
+# output_file_2.close()
 input_file.close()
 
 ####
@@ -52,13 +52,13 @@ import hashlib
 
 input_file_path = "V2/Trojan.txt"
 output_file_path_1 = "TrojanL.txt"
-output_file_path_2 = "TrojanF.txt"
+# output_file_path_2 = "TrojanF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
 output_file_1 = open(output_file_path_1, "w")
-output_file_2 = open(output_file_path_2, "w")
+# output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
     hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
@@ -68,7 +68,7 @@ for line in input_file:
         completed_lines_hash.add(hashValue)
 
 output_file_1.close()
-output_file_2.close()
+# output_file_2.close()
 input_file.close()
 
 ####
@@ -77,13 +77,13 @@ import hashlib
 
 input_file_path = "V2/Vmess.txt"
 output_file_path_1 = "VmessL.txt"
-output_file_path_2 = "VmessF.txt"
+# output_file_path_2 = "VmessF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
 output_file_1 = open(output_file_path_1, "w")
-output_file_2 = open(output_file_path_2, "w")
+# output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
     hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
@@ -93,7 +93,7 @@ for line in input_file:
         completed_lines_hash.add(hashValue)
 
 output_file_1.close()
-output_file_2.close()
+# output_file_2.close()
 input_file.close()
 
 ####
@@ -102,13 +102,13 @@ import hashlib
 
 input_file_path = "V2/Vless.txt"
 output_file_path_1 = "VlessL.txt"
-output_file_path_2 = "VlessF.txt"
+# output_file_path_2 = "VlessF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
 output_file_1 = open(output_file_path_1, "w")
-output_file_2 = open(output_file_path_2, "w")
+# output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
     hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
@@ -118,7 +118,7 @@ for line in input_file:
         completed_lines_hash.add(hashValue)
 
 output_file_1.close()
-output_file_2.close()
+# output_file_2.close()
 input_file.close()
 
 ####
@@ -134,7 +134,7 @@ import hashlib
 
 input_file_path = "V2/Hy2.txt"
 output_file_path_1 = "Hy2L.txt"
-output_file_path_2 = "Hy2F.txt"
+# output_file_path_2 = "Hy2F.txt"
 
 completed_lines_hash = set()
 
@@ -146,11 +146,11 @@ for line in input_file:
     hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
     if hashValue not in completed_lines_hash:
         output_file_1.write(line)
-        output_file_2.write(line)
+        # output_file_2.write(line)
         completed_lines_hash.add(hashValue)
 
 output_file_1.close()
-output_file_2.close()
+# output_file_2.close()
 input_file.close()
 
 ####
@@ -165,13 +165,13 @@ import hashlib
 
 input_file_path = "V2/Tuic.txt"
 output_file_path_1 = "TuicL.txt"
-output_file_path_2 = "TuicF.txt"
+# output_file_path_2 = "TuicF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
 output_file_1 = open(output_file_path_1, "w")
-output_file_2 = open(output_file_path_2, "w")
+# output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
     hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
@@ -181,7 +181,7 @@ for line in input_file:
         completed_lines_hash.add(hashValue)
 
 output_file_1.close()
-output_file_2.close()
+# output_file_2.close()
 input_file.close()
 
 ####
@@ -193,13 +193,13 @@ import hashlib
 
 input_file_path = "V2/Hysteria.txt"
 output_file_path_1 = "HysteriaL.txt"
-output_file_path_2 = "HysteriaF.txt"
+# output_file_path_2 = "HysteriaF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
 output_file_1 = open(output_file_path_1, "w")
-output_file_2 = open(output_file_path_2, "w")
+# output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
     hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
@@ -209,7 +209,7 @@ for line in input_file:
         completed_lines_hash.add(hashValue)
 
 output_file_1.close()
-output_file_2.close()
+# output_file_2.close()
 input_file.close()
 
 ####
@@ -223,7 +223,7 @@ import hashlib
 
 input_file_path = "V2/juicity.txt"
 output_file_path_1 = "juicityL.txt"
-output_file_path_2 = "juicityF.txt"
+# output_file_path_2 = "juicityF.txt"
 
 completed_lines_hash = set()
 
@@ -235,11 +235,11 @@ for line in input_file:
     hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
     if hashValue not in completed_lines_hash:
         output_file_1.write(line)
-        output_file_2.write(line)
+        # output_file_2.write(line)
         completed_lines_hash.add(hashValue)
 
 output_file_1.close()
-output_file_2.close()
+# output_file_2.close()
 input_file.close()
 
 ####
